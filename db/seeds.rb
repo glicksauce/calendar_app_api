1f20380503e931697297e34626673b9becbe9bb0
Jay Glickman <jayglickman@gmail.com>
2020-05-12 22:24:26 -0400

    better seed date

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,15 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Calendar.create([
-  { calendar_date: '2020-May-06'.to_date },
-  { calendar_date: '2020-May-07'.to_date },
-  { calendar_date: '2020-May-08'.to_date }
-])
+seedDate = '2020-Jan-01'.to_date
+
+while seedDate <= '2020-12-31'.to_date
+  Calendar.create([
+  { calendar_date: seedDate }
+  ])
+
+  puts seedDate
+  seedDate += 1
+end
 
 Photo.create([
-    { img_date: '2020-May-06'.to_date, img_src: "meh", journal_entry: "feeling meh about this picture"},
-    { img_date: '2020-May-06'.to_date, img_src: "bleh"},
-    { img_date: '2020-May-07'.to_date, img_src: "cheh"}
-
+    { img_date: '2020-May-13'.to_date, img_src: "https://thumbs.gfycat.com/TenseSecondAnophelesmosquito-mobile.mp4", journal_entry: "Ready for project 4 presentation!"},
+    { img_date: '2020-May-13'.to_date, img_src: "https://cdn5.vectorstock.com/i/1000x1000/38/74/good-vibes-vector-21113874.jpg", journal_entry: "Good Vibes!"},
+    { img_date: '2020-May-14'.to_date, img_src: "https://toggl.com/blog/wp-content/uploads/2014/12/01-excited-programmer-comic.png", journal_entry "pumped to start working on my final project"}
+    { img_date: '2020-May-28'.to_date, img_src: "https://communityimpact.com/uploads/images/2020/05/08/56133.jpg", journal_entry "Graduating GA today, bittersweet"}
 ])
+
+
